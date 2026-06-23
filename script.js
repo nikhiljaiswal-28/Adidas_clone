8f8391e89f7d118d20bfb4eecc2c494e43e93211
Nikhil Jaiswal <142736822+nikhiljaiswal-28@users.noreply.github.com>
2026-06-23 16:55:18 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,16 @@
 $(document).ready(function(){
 
-let cartCount=0;
+var cartCount = 0;
 
 $(".add-cart").click(function(){
 
-cartCount++;
+    cartCount = cartCount + 1;
 
-$("#cartCount").text(cartCount);
+    $("#cartCount").text(cartCount);
 
-alert("Item Added Successfully!");
+    alert("Item Added Successfully!");
 
-$(this).text("Added ✓");
-
-$(this).removeClass("btn-dark");
-
-$(this).addClass("btn-success");
+    $(this).text("Added");
 
 });
 
